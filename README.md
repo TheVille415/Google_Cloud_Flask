93cde275da02b8ca407afdcc39e6a54aa4cf0226
Jordan_Torres <jt.arontorres@gmail.com>
2022-05-11 03:18:07 -0700

    app pushed

---
+++ README.md
@@ -0,0 +1,5 @@
+# GoogleCloud Flask Docker App
+
+For this tutorial, we will be using the Google Cloud Platform to create a Flask Docker App.
+
+I followed this tutorial: [https://mlhive.com/2022/02/deploy-deep-learning-models-using-flask-docker-and-github-on-google-cloud] to create this Docker App.
---
+++ main.py
@@ -0,0 +1,28 @@
+# import required modules
+from flask import Flask, render_template, request
+
+# create flask app
+app = Flask(__name__)
+
+# main route (show html page)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# api endpoint for image upload
+@app.route('/api/upload', methods=['POST'])
+def upload():
+    # receive the file from the client
+    file = request.files['file']
+    filepath = f'static/temp/{file.filename}'
+    file.save(filepath) # save to directory
+    
+    # process image after upload
+    process_image(filepath)
+    
+    # return server url to client
+    return f"{request.url_root}{filepath}"
+
+# Run flask server
+if __name__ == '__main__':
+    app.run(debug=True) # set debug true to load reload server auto on changes
